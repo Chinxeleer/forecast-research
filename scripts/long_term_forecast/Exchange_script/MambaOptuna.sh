6fccb291eb9360d1a2f3f07d94be97dc713f4f39
chinxeleer <blessingeleer.programming@gmail.com>
2025-10-15 15:44:07 +0200

    Trying to use optuna for parameter tuning

---
+++ scripts/long_term_forecast/Exchange_script/MambaOptuna.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+export CUDA_VISIBLE_DEVICES=0
+model_name=Mamba
+for pred_len in 1 7 14 30; do
+
+	python -u optuna_tune.py \
+		--task_name long_term_forecast \
+		--is_training 1 \
+		--root_path ./dataset/exchange_rate/ \
+		--data_path exchange_rate.csv \
+		--model_id Exchange_$pred_len \
+		--model $model_name \
+		--data custom \
+		--study_name Mamba_Exchange_$pred_len \
+		--n_trials 10 \
+		--features M \
+		--seq_len 96 \
+		--label_len 5 \
+		--pred_len $pred_len \
+		--e_layers 2 \
+		--d_layers 1 \
+		--enc_in 5 \
+		--expand 2 \
+		--d_ff 16 \
+		--dec_in 5 \
+		--d_conv 4 \
+		--c_out 5 \
+		--d_model 128 \
+		--des 'Exp' \
+		--itr 1 # --patience 100 \
+	# --train_epochs 50 \
+
+done
